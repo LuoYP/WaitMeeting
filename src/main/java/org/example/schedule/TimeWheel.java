fcc443a4ad67d45fc6816c9f9e682deb86237327
luoyp <1783508363@qq.com>
2023-05-31 22:11:14 +0800

    自定义简单的时间轮管理定时任务

--- src/main/java/org/example/schedule/TimeWheel.java
+++ src/main/java/org/example/schedule/TimeWheel.java
@@ -1,47 +1,106 @@
 package org.example.schedule;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 /**
  * 时间轮管理定时任务
  */
 public class TimeWheel {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TimeWheel.class);
+
     //每轮默认刻度:60
     private static final int DEFAULT_TICKS_PER_WHEEL = 60;
 
-    //每个刻度默认步长:1s
+    //每个刻度默认步长:1ms
     private static final int DEFAULT_TICK_DURATION = 1;
+
+    //执行定时任务的线程池数量: 默认30(线程池还可优化)
+    private static final int DEFAULT_WORK_THREADS = 30;
 
     private final int ticksPerWheel;
 
     private final int tickDuration;
 
     //槽,用来存放任务的环形数组
-    private TimeWheelTaskList[] timeWheelTaskLists;
+    private LinkedList<TimeWheelTask>[] timeWheelTaskLinks;
 
     //环形数组的指针
     private volatile int currentTickIndex = 0;
 
     private long startTime;
 
+    private ExecutorService bossGroup;
+
+    private ExecutorService workGroup;
+
     public TimeWheel() {
-        this(DEFAULT_TICKS_PER_WHEEL, DEFAULT_TICK_DURATION);
+        this(DEFAULT_TICKS_PER_WHEEL, DEFAULT_TICK_DURATION, DEFAULT_WORK_THREADS);
     }
 
-    public TimeWheel(int ticksPerWheel, int tickDuration) {
+    public TimeWheel(int ticksPerWheel, int tickDuration, int workThreadNumbers) {
         this.ticksPerWheel = ticksPerWheel;
         this.tickDuration = tickDuration;
         this.startTime = System.currentTimeMillis();
-        timeWheelTaskLists = new TimeWheelTaskList[ticksPerWheel];
+        timeWheelTaskLinks = new LinkedList[ticksPerWheel];
         for (int i = 0; i < ticksPerWheel; i++) {
-            timeWheelTaskLists[0] = new TimeWheelTaskList();
+            timeWheelTaskLinks[i] = new LinkedList<>();
         }
+        bossGroup = Executors.newFixedThreadPool(1);
+        workGroup = Executors.newFixedThreadPool(workThreadNumbers);
     }
 
     public void start() {
+        bossGroup.execute(() -> {
+            while (true) {
+                try {
+                    //遍历当前槽是否有在当前周期中快要过期的任务
+                    LinkedList<TimeWheelTask> timeWheelTaskLink = timeWheelTaskLinks[currentTickIndex];
+                    Iterator<TimeWheelTask> iterator = timeWheelTaskLink.iterator();
+                    while (iterator.hasNext()) {
+                        TimeWheelTask task = iterator.next();
+                        long scheduleTime = task.scheduleTime();
+                        if (scheduleTime - System.currentTimeMillis() <= tickDuration) {
+                            workGroup.submit(task.runnable());
+                            iterator.remove();
+                        }
+                    }
+                    currentTickIndex = (currentTickIndex + 1) % ticksPerWheel;
+                    TimeUnit.MILLISECONDS.sleep(tickDuration);
+                } catch (InterruptedException e) {
+                    LOGGER.error(e.getMessage());
+                }
+            }
+        });
 
     }
 
-    public void addTask() {
+    public void stop() {
+        bossGroup.shutdown();
+        workGroup.shutdown();
+    }
 
+    public boolean addTask(TimeWheelTask timeWheelTask) {
+        long scheduleTime = timeWheelTask.scheduleTime();
+        if (scheduleTime < startTime) {
+            //已过期
+            return false;
+        }
+        //根据超时时间将任务分配到对应的槽
+        if (scheduleTime < startTime + tickDuration) {
+            timeWheelTaskLinks[currentTickIndex].add(timeWheelTask);
+        } else {
+            int slot = (int) (scheduleTime - startTime) / tickDuration;
+            int taskIndex = slot % ticksPerWheel;
+            timeWheelTaskLinks[taskIndex].add(timeWheelTask);
+        }
+        return true;
     }
 }
--- src/main/java/org/example/schedule/TimeWheelTask.java
+++ src/main/java/org/example/schedule/TimeWheelTask.java
@@ -1,9 +1,21 @@
 package org.example.schedule;
 
-public class TimeWheelTask implements Runnable{
+public class TimeWheelTask {
 
-    @Override
-    public void run() {
+    private long scheduleTime;
 
+    private Runnable runnable;
+
+    public TimeWheelTask(long scheduleTime, Runnable runnable) {
+        this.scheduleTime = scheduleTime;
+        this.runnable = runnable;
+    }
+
+    public long scheduleTime() {
+        return scheduleTime;
+    }
+
+    public Runnable runnable() {
+        return runnable;
     }
 }
